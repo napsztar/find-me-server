b2cd372d639261aec96aec49cae31807ea93db3f
Outwater <kocon135@gmail.com>
2021-03-17 23:25:23 +0900

    ADD prettier for code convention

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require('cors');
+const morgan = require('morgan');
 
 app.use(cors());
+let func = function () {
+  let foo = 'abc11';
+  return foo;
+};
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-})
+  res.send('hello world');
+});
 
 app.listen(port, () => {
-    console.log(`server start at ${port}`)
-})
+  console.log(`server start at ${port}`);
+});
