38a7ac38826a08d13933ab57e5522cdcd82b6520
Monocrom1018 <74134503+Monocrom1018@users.noreply.github.com>
2021-03-19 11:28:22 +0900

    Add Routes.js to clarify routing

--- app.js
+++ app.js
@@ -4,48 +4,21 @@ const port = 5000;
 const cors = require('cors');
 const morgan = require('morgan');
 const controller = require('./controllers');
+const router = require('./Routes');
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 // 홈
 app.get('/', (req, res) => {
-  res.send("Hello world!");
+  res.status(200).send('Hello world!');
 });
 
-// 유저인포
-app.get('/users', (req, res) => {
-  res.status(200).json({
-    id: "PK",
-    password: "password",
-    email: "email",
-    nickname : "nickname",
-    });
-});
-
-// 로그인
-app.post('/users/signin', controller.signin);
-
-// 로그아웃
-app.post('/users/signout', controller.signout);
-
-// 회원가입
-app.post('/users/signup', controller.signup);
-
-// 질문-대답세트
+app.use('/users', router);
 app.get('/answer/:userid', controller.answer);
-
-// 질문
 app.get('/question', controller.question);
-
-// 비밀번호 변경
-app.post('/users/:userid/update', controller.update);
-
-// 회원탈퇴
-app.delete('/users/:userid/delete', controller.delete);
 
 app.listen(port, () => {
   console.log(`server start at ${port}`);
