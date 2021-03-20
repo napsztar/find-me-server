8edd1c20113ed24434430e01fedebc759198aac5
Outwater <kocon135@gmail.com>
2021-03-20 19:03:49 +0900

    Fix api dummy requests

--- controllers/index.js
+++ controllers/index.js
@@ -3,26 +3,53 @@ const { answer, question, user } = models;
 module.exports = {
   //--------------------------------------------------------------------------------
   answer: async (req, res) => {
-    const userId = req.params.userid;
-    if (!userId) {
-      return res.status(401).send('Unauthorized user.');
-    } else {
-      const result = await answer.findAll({
-        include: [
-          {
-            model: question,
-          },
-          { model: user, where: { id: userId }, attributes: ['nickname'] },
-        ],
-      });
-      res.status(200).json(result);
-    }
+    const result = [
+      {
+        'answer-id': 1,
+        'question-content': '내 삶의 목적은 무엇인가?',
+      },
+      {
+        'answer-id': 2,
+        'question-content': '사람은 변할 수 있을까?',
+      },
+      {
+        'answer-id': 3,
+        'question-content': '최근에 읽고 있는 글이나 책이 있다면?',
+      },
+    ];
+    res.status(200).json(result);
   },
   //--------------------------------------------------------------------------------
-  question: async (req, res) => {
-    const result = await question.findOne({
-      where: { NthDay: 1 },
+  addAnswer: (req, res) => {
+    res.status(200).json({ message: 'Success' });
+  },
+  //--------------------------------------------------------------------------------
+  readAnswer: (req, res) => {
+    const result = {
+      questionId: 1,
+      questionContent: '내 삶의 목적은 무엇인가?',
+      answerId: 1,
+      createdAt: '2020-03-18T16:22:32.000',
+      updatedAt: '2020-03-18T16:22:32.000',
+      questionedAt: '2020-03-19T16:22:32.000',
+    };
+    res.status(200).json(result);
+  },
+  //--------------------------------------------------------------------------------
+  editAnswer: (req, res) => {
+    res.status(200).json({
+      message: 'answer is successfully updated',
     });
+  },
+  //--------------------------------------------------------------------------------
+
+  question: (req, res) => {
+    const result = {
+      questionId: 1,
+      questionContent: '내 삶의 목적은 무엇인가?',
+      answerId: 1,
+      questionAt: '2020-03-18T16:22:32.000Z',
+    };
     res.status(200).json(result);
   },
   //--------------------------------------------------------------------------------
